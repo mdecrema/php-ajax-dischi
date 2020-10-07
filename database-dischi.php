295bdddee084abb0bb4fbefdbe7bd596bd0216bf
marco <marcodecrema@libero.it>
2020-10-07 17:29:55 +0200

    cotenuto database

--- database-dischi.php
+++ database-dischi.php
@@ -2,39 +2,75 @@
 
 $dischi = [
   [
-    "picture" => "",
+    "picture" => "sulta.jpg",
+    "title" => "sulta selects",
+    "author" => "Denis Sulta",
+    "year" => "1991"
+  ],
+  [
+    "picture" => "pendergrass.jpg",
+    "title" => "Teddy",
+    "author" => "Jamie Jones",
+    "year" => "1991"
+  ],
+  [
+    "picture" => "trick.jpg",
     "title" => "Sugar Free",
     "author" => "Red Hot Chilli Peppers",
     "year" => "1991"
   ],
   [
-    "picture" => "",
+    "picture" => "garden.jpg",
     "title" => "Sugar Free",
     "author" => "Red Hot Chilli Peppers",
     "year" => "1991"
   ],
   [
-    "picture" => "",
-    "title" => "Sugar Free",
-    "author" => "Red Hot Chilli Peppers",
+    "picture" => "sun.jpg",
+    "title" => "the sun",
+    "author" => "Denis Cruz",
     "year" => "1991"
   ],
   [
-    "picture" => "",
-    "title" => "Sugar Free",
-    "author" => "Red Hot Chilli Peppers",
+    "picture" => "watergate.jpg",
+    "title" => "watergate",
+    "author" => "Salomun",
     "year" => "1991"
   ],
   [
-    "picture" => "",
-    "title" => "Sugar Free",
-    "author" => "Red Hot Chilli Peppers",
+    "picture" => "line.jpg",
+    "title" => "see line",
+    "author" => "Dennis Cruz",
     "year" => "1991"
   ],
   [
-    "picture" => "",
-    "title" => "Sugar Free",
-    "author" => "Red Hot Chilli Peppers",
+    "picture" => "aleksandir.jpg",
+    "title" => "hard to explain",
+    "author" => "Aleksandir",
+    "year" => "1991"
+  ],
+  [
+    "picture" => "do.jpg",
+    "title" => "",
+    "author" => "Raffa FL",
+    "year" => "1991"
+  ],
+  [
+    "picture" => "one.jpg",
+    "title" => "one",
+    "author" => "Ben Klock ",
+    "year" => "1991"
+  ],
+  [
+    "picture" => "fabric.jpg",
+    "title" => "fabric",
+    "author" => "Amelie Lens",
+    "year" => "1991"
+  ],
+  [
+    "picture" => "hero.jpg",
+    "title" => "hero's back",
+    "author" => "Ilario Alicante",
     "year" => "1991"
   ]
 ]
--- index.php
+++ index.php
@@ -7,29 +7,33 @@
     <link rel="stylesheet" href="dist/app.css">
   </head>
     <body>
+      <!-- Navigation bar -->
       <header>
         <div class="container">
           <img src="img/logo.png" alt="logo" />
-            <!--<div class="generiMusicali">
-                  <select id="genere" class="">
-                    <option class="type" value="0">--Scegli Genere--</option>
-                    <option class="type" value="1">Pop</option>
-                    <option class="type" value="2">Rock</option>
-                    <option class="type" value="3">Metal</option>
-                    <option class="type" value="4">Jazz</option>
-                  </select>
-                  <button class="btn-invio" id="invio" type="submit">Cerca</button>
-                </div>-->
-              </div>
+      <!--<div class="generiMusicali">
+            <select id="genere" class="">
+              <option class="type" value="0">--Scegli Genere--</option>
+              <option class="type" value="1">Pop</option>
+              <option class="type" value="2">Rock</option>
+              <option class="type" value="3">Metal</option>
+              <option class="type" value="4">Jazz</option>
+            </select>
+            <button class="btn-invio" id="invio" type="submit">Cerca</button>
+          </div>-->
+        </div>
         </header>
+        <!-- /Navigation bar -->
+        <!-- Contenuto principale pagina -->
       <main>
+        <!-- Contenitore lista album -->
         <div class="dischi container">
-            <!-- Disco ad esempio -->
+            <!-- Contenitore disco -->
           <?php include 'database-dischi.php';
             foreach ($dischi as $key) {
           ?>
           <div class="cd">
-            <img src="<?php echo $key["picture"] ?>" alt="">
+            <img src="img/<?php echo $key["picture"] ?>" alt="">
             <h3><?php echo $key["title"] ?></h3>
             <span class="author"><?php echo $key["author"] ?></span>
             <span class="year"><?php echo $key["year"] ?></span>
@@ -39,5 +43,6 @@
           ?>
         </div>
       </main>
+      <!-- /Contenuto principale pagina -->
     </body>
   </html>
