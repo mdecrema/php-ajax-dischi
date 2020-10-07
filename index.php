a9b8116c7188f9f86e8eac18e1e46af048b6dcaf
marco <marcodecrema@libero.it>
2020-10-07 16:24:24 +0200

    html e css

--- index.php
+++ index.php
@@ -3,8 +3,35 @@
   <head>
     <meta charset="utf-8">
     <title>Dischi</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="dist/app.css">
   </head>
-  <body>
-    <h1>Dischi php</h1>
-  </body>
-</html>
+    <body>
+      <header>
+        <div class="container">
+          <img src="img/logo.png" alt="logo" />
+            <!--<div class="generiMusicali">
+                  <select id="genere" class="">
+                    <option class="type" value="0">--Scegli Genere--</option>
+                    <option class="type" value="1">Pop</option>
+                    <option class="type" value="2">Rock</option>
+                    <option class="type" value="3">Metal</option>
+                    <option class="type" value="4">Jazz</option>
+                  </select>
+                  <button class="btn-invio" id="invio" type="submit">Cerca</button>
+                </div>-->
+              </div>
+        </header>
+      <main>
+        <div class="cds-container container">
+            <!-- Disco ad esempio -->
+            <div class="cd">
+                <img src="https://upload.wikimedia.org/wikipedia/en/9/9f/Ten_Summoner%27s_Tales.jpg" alt="">
+                <h3>Ten's Summoners Tales</h3>
+                <span class="author">Sting</span>
+                <span class="year">1993</span>
+            </div>
+        </div>
+      </main>
+    </body>
+  </html>
